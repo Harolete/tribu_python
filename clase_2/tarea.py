c72564d89f12393f25a31e23002c6e76acbd0fe1
Harold_Pestano <pestanoh@globalhitss.com>
2021-10-03 22:11:46 -0300

    organizando el repositorio

---
+++ clase_2/tarea.py
@@ -0,0 +1,84 @@
+print('\n','1) ')
+# 1) Cuardar en una variable un entero y en otra un número decimal
+#       e imprimir la suma de ambos
+
+print('\n','2) ')
+# 2) Calcular el resultado de un entero elevado a la potencia de si mismo e imprimirlo
+#       ejemplo: 3³
+
+print('\n','3) ')
+# 3) Calcular la raíz cuadrada de 144 e imprimirla 
+
+print('\n','4) ')
+# 4) Convertir el decimal -5.76 en un entero positivo redondeado, es decir 6
+
+print('\n','5) ')
+# 5) Imprimir el tipo de dato de cada uno de los resultados anteriores
+
+print('\n','6) ')
+# 6) Imprimir la primera letra del siguiente string
+mi_string = "solteroski"
+
+print('\n','7) ')
+# 7) Imprimir unicamente el mensaje escondido
+mi_mensaje = "ranujvasnvuirabjabvuiarvjuamaricoelqueloleanciuaonrgoavibaiec"
+
+print('\n','8) ')
+# 8) Imprimir la longitud de mi_mensaje y la longitud del mensaje oculto
+
+print('\n','9) ')
+# 9) Imprimir que pasa si las bolas no se tocan (no se vale escribir otro string Cesar)
+# Solo pueden usar "maricoteo"
+maricoteo = 'si las bolas no se tocan, no es homosexualidad'
+
+print('\n','10) ')
+# 10) Imprimir el primer elemento de un mi_lista
+mi_lista = [0,1,2,3,4]
+
+print('\n','11) ')
+# 11) Imprimir el ultimo elemento de mi_lista
+
+print('\n','12) ')
+# 12) Imprimir  entre la 4ta posición y las 9na posición 
+mi_otra_lista = [0,1,2,3,4,5,6,7,8,9,10]
+
+print('\n','13) ')
+# 13) Imprimir la longitud de mi_otra_lista 
+
+print('\n','14) ')
+# 14) Tomar palabras de la siguiente lista y concatenarlas para formar
+#       una frase e imprimirla como un solo string.
+palabras = [
+            'etesech',
+            'el pepe',
+            'juancalo',
+            'es',
+            'argentina',
+            'el',
+            'rey',
+            'un',
+            'mamawebazo',
+            'maicol',
+            'del pop',
+            'chupa pija',
+            'otaku',
+            'bañese',
+            'en',
+            'caracas',
+            'tribu',
+            'la',
+            'y'
+            ]
+
+print('\n','15) ')
+# 15) Crear un diccionario con las key nombre, apellido, edad, nacionalidad,sexo
+#       e imprimirlo (no Cesar, "imprimirlo" no es otra key. TIENES QUE IMPRIMIRLO)
+
+print('\n','16) ')
+# 16) Imprimir por separado cada key del diccionario anterior
+
+# 17) comentar las diferencias y similitudes entre:
+    # Listas
+    # Tuplas
+    # Diccionarios
+    # Strings
